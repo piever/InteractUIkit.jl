bc573e8949ba6474978a140486f7954de8d7cac6
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-03 21:07:04 +0100

    toggle class

--- src/widgets.jl
+++ src/widgets.jl
@@ -13,7 +13,7 @@ function toggle(::UIkit; class="", label = "", outer=dom"div", kwargs...)
     outerfunc = function (args...)
         outer(dom"div.uk-inline"(dom"div.uk-onoffswitch"(args...)), dom"div.uk-inline"(dom"label.uk-text"(label)))
     end
-    cb = checkbox(NativeHTML(); outer = outerfunc, label = (dom"span.uk-onoffswitch-inner"(), dom"span.uk-onoffswitch-switch"()),
+    cb = toggle(NativeHTML(); outer = outerfunc, label = (dom"span.uk-onoffswitch-inner"(), dom"span.uk-onoffswitch-switch"()),
         class="uk-onoffswitch-checkbox $class", labelclass = "uk-onoffswitch-label", kwargs...)
 end
 
