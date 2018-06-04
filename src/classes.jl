2fe3769df3c9a4299bd355f0b68f006e8a63dbad
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-04 13:06:31 +0100

    switched to class style

---
+++ src/classes.jl
@@ -0,0 +1,23 @@
+function getclass(T::UIkit, arg, typ...)
+    length(typ) > 0 && last(typ) == "fullwidth" && return ""
+    length(typ) > 0 && last(typ) == "label" && return (typ[1] == "toggle") ? "uk-onoffswitch-label" : ""
+
+    if arg == :input
+        typ==("checkbox",) && return "uk-checkbox"
+        typ==("toggle",) && return "uk-toggle"
+        typ==("range",) && return "uk-range"
+
+        return "uk-input"
+    elseif arg == :button
+        typ==("primary",) && return "uk-button-primary"
+        return "uk-button"
+    elseif arg == :textarea
+        return "uk-textarea"
+    elseif arg==:wdglabel
+        return ""
+    elseif arg==:dropdown
+        return "uk-select"
+    else
+        return ""
+    end
+end
--- src/widgets.jl
+++ src/widgets.jl
@@ -1,24 +1,10 @@
-filepicker(::UIkit, args...; class="", kwargs...) =
-    filepicker(NativeHTML(), args...; class="uk-input $class", kwargs...)
-
-dropdown(::UIkit, options::Associative; class="", kwargs...) =
-    dropdown(NativeHTML(), options; class="uk-select $class", kwargs...)
-
-function entry(::UIkit, style, args...; class="", kwargs...)
-    extraclass = "uk-checkbox"
-    entry(NativeHTML(), style, args...; class="$extraclass $class", kwargs...)
-end
-
 function toggle(::UIkit; class="", label = "", outer=dom"div", kwargs...)
     outerfunc = function (args...)
         outer(dom"div.uk-inline"(dom"div.uk-onoffswitch"(args...)), dom"div.uk-inline"(dom"label.uk-text"(label)))
     end
     cb = toggle(NativeHTML(); outer = outerfunc, label = (dom"span.uk-onoffswitch-inner"(), dom"span.uk-onoffswitch-switch"()),
-        class="uk-onoffswitch-checkbox $class", labelclass = "uk-onoffswitch-label", kwargs...)
+        class="uk-onoffswitch-checkbox $class", kwargs...)
 end
-
-button(::UIkit, args...; class= "uk-button-primary", kwargs...) =
-    button(NativeHTML(), args...; class="uk-button $class", kwargs...)
 
 togglebuttons(::UIkit, options::Associative; class="uk-width-1-1", outer = identity, activeclass = "uk-button-primary", outer_attributes = Dict(), kwargs...) =
     togglebuttons(NativeHTML(), options;
@@ -34,25 +20,3 @@ radiobuttons(T::UIkit, options::Associative; outer = identity, outer_attributes 
     radiobuttons(NativeHTML(), options::Associative;
         outer = outer∘Node(:div, className = "uk-margin uk-grid-small uk-child-width-auto uk-grid", attributes = outer_attributes),
         kwargs...)
-
-radio(T::UIkit, s, key, val, vmodel; class = "", kwargs...) =
-    Node(:label, dom"input[class=uk-radio $class name = $s, type=radio, $vmodel=value, value=$val]"(), key,
-        attributes = Dict(kwargs))
-
-function input(::UIkit, args...; class="", typ="text", kwargs...)
-    extra_class =
-        if typ=="radio"
-            "uk-radio"
-        elseif typ=="checkbox"
-            "uk-checkbox"
-        elseif typ=="range"
-            "uk-range"
-        else
-            "uk-input"
-        end
-
-    input(NativeHTML(), args...; class= extra_class * " " * class, typ=typ, kwargs...)
-end
-
-textarea(::UIkit, args...; class="", kwargs...) =
-    textarea(NativeHTML(), args...; class="uk-textarea $class", kwargs...)
