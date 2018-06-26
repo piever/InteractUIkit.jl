7425bf1accb415659309d191b4cfcc9743055f94
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-26 17:55:38 +0100

    fix toggle (#7)

--- src/widgets.jl
+++ src/widgets.jl
@@ -1,7 +1,11 @@
-function toggle(::UIkit; className="", label = "", outer=dom"div", kwargs...)
-    outerfunc = function (args...)
-        outer(dom"div.uk-inline"(dom"div.uk-onoffswitch"(args...)), dom"div.uk-inline"(dom"label.uk-text"(label)))
-    end
-    cb = toggle(NativeHTML(); outer = outerfunc, label = (dom"span.uk-onoffswitch-inner"(), dom"span.uk-onoffswitch-switch"()),
-        className="uk-onoffswitch-checkbox $className", kwargs...)
+function toggle(::UIkit; value=false, label="", bind="checked", valueUpdate="change", kwargs...)
+    ip = input(NativeHTML(), value; typ="checkbox", wdgtyp="toggle", bind=bind, valueUpdate="change", kwargs...)
+    InteractBase.scope(ip).dom = dom"div.field"(
+        Node(:label, className = "switch-light switch-material", attributes = Dict("onclick"=>""))(
+            InteractBase.scope(ip).dom,
+            label,
+            Node(:span, Node(:span, "Off"), Node(:span, "On"), Node(:a))
+        )
+    )
+    Widget{:toggle}(ip)
 end
